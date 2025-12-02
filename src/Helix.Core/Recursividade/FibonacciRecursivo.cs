bf1b77306dc2deb384219881a2d3e61860014c37
Renzzo Rocha <renzzorocha17@gmail.com>
2025-12-02 17:12:12 -0300

    Adiciona cálculo recursivo de Fibonacci
    Foi adicionada a classe `FibonacciRecursivo` no namespace
    `Helix.Core.Recursividade`. Essa classe implementa o método
    estático `Fibonacci`, que calcula números de Fibonacci de
    forma recursiva. O método retorna `0` para `n = 0`, `1` para
    `n = 1` e a soma de `Fibonacci(n - 1)` e `Fibonacci(n - 2)`
    para outros valores de `n`.
    
    Itens de Trabalho Relacionados: #7

---
+++ src/Helix.Core/Recursividade/FibonacciRecursivo.cs
@@ -0,0 +1,13 @@
+﻿namespace Helix.Core.Recursividade
+{
+    internal class FibonacciRecursivo
+    {
+        public static int Fibonacci(int n)
+        {
+            if(n == 0) { return 0; }
+            if(n == 1) { return 1; }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
